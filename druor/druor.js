162cd8c22a6346972984ee434589abf2268efac8
Sanjay Rohila <sanjay@srijan.in>
2015-10-10 16:27:14 +0530

    sharejs editor integration

--- druor/druor.js
+++ druor/druor.js
@@ -1,5 +1,31 @@
+Docs = new Meteor.Collection("nodes");
+
 if (Meteor.isClient) {
 
+  Template.editor.helpers({
+    docid: function() {
+      return "1";
+    },
+    title: function() {
+      return (ref = Docs.findOne("1")) != null ? ref.title : void 0;
+    }
+  });
+
+  Template.editor.events = {
+    "keydown #editor": function(e) {
+      var id = "1";
+      if (e.keyCode !== 13) {
+        return;
+      }
+      e.preventDefault();
+      $(e.target).blur();
+      var title = (ref = Docs.findOne("1")) != null ? ref.title : void 0;
+      return Docs.update(id, {
+        title: title,
+        body: e.target.value
+      });
+    }
+  };
 }
 
 if (Meteor.isServer) {
