69fe47635c1937843c5112cfa498b38fdf5be8d0
Sanjay Rohila <sanjay@srijan.in>
2015-10-11 23:21:05 +0530

    Update drupal node into mongoDB and update mongo via sharejs

--- druor/client/app.js
+++ druor/client/app.js
@@ -1,24 +1,54 @@
 var nid = 1;
+// Meteor.call('getNodeFromUrl', "http://dev-druor.pantheon.io/rest/view/node/1", function(err, data) {
+//   var keys = [];
+//   var node = JSON.parse(data);
+//   Meteor.call("get", node.nid, function(err, data) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     node = data;
+//   });
+//   Meteor.call('insert', data);
+// });
 Meteor.call("get", nid, function(err, data) {
   if (err) {
     console.log(err);
   }
-  Session.set('node', data);
+  if (data && data.nid) {
+    Session.set('node', data);
+  }
+  else {
+    Meteor.call('getNodeFromUrl', 'http://dev-druor.pantheon.io/rest/view/node/'+nid, function(err, data) {
+      if (err) {
+        return;
+      }
+      var keys = [];
+      var node = JSON.parse(data);
+      Meteor.call('insert', node);
+      Session.set('node', node);
+    });
+  }
 });
 
 Template.edit.helpers({
   title: function() {
     return Session.get('node').title;
+  },
+  body: function() {
+    return Session.get('node').body['und'][0]['value'];
   }
 });
 
 Template.editor.helpers({
-  docid: function() {
+  nid: function() {
     return nid.toString();
   }
 });
 
 Template.edit.events = {
+  "ready document": function(e) {
+    alert('sdaff');
+  },
   "keydown #editor": function(e) {
     if (e.keyCode !== 27) {
       return;
@@ -29,8 +59,25 @@ Template.edit.events = {
     if ($('#title').val() != node.title) {
       node.title = $('#title').val();
     }
-    node.body = e.target.value;
+    node.body['und'][0]['value'] = e.target.value;
+    node.body['und'][0]['safe_value'] = e.target.value;
     Meteor.call('update', node);
     return;
-  }
+  },
+  // "click #submit": function(e) {
+  //   if (e.keyCode !== 27) {
+  //     return;
+  //   }
+  //   var node = Session.get('node');
+  //   e.preventDefault();
+  //   $(e.target).blur();
+  //   if ($('#title').val() != node.title) {
+  //     node.title = $('#title').val();
+  //   }
+  //   node.body['und'][0]['value'] = $('#editor').val();
+  //   node.body['und'][0]['safe_value'] = $('#editor').val();
+  //   Meteor.call('update', node);
+  //   Meteor.call('updateDrupal', "http://dev-druor.pantheon.io/rest/update/node/", node);
+  //   return;
+  // }
 };
--- druor/server/app.js
+++ druor/server/app.js
@@ -1,31 +1,39 @@
 Docs = new Mongo.Collection("nodes");
 Meteor.methods({
-  insertRecords: function(url) {
-      console.log(url);
-      // Docs.insert({
-      //   _id: data.nid.toString(),
-      // });
+  getNodeFromUrl: function(url) {
+    this.unblock();
+    var result = Meteor.http.call("GET", url);
+    if (result.statusCode == 200) {
+      return result.content;
+    }
   },
   getTitle: function(nid) {
-    return (ref = Docs.findOne({nid: nid})) != null ? ref.title : void 0;
+    if (typeof(nid != "string")) {
+      nid = nid.toString();
+    }
+    return (ref = Docs.findOne({_id: nid})) != undefined ? ref.node.title : void 0;
   },
   get: function(nid) {
-    return Docs.findOne({nid: nid});
+    if (typeof(nid != "string")) {
+      nid = nid.toString();
+    }
+    var result = Docs.findOne({_id: nid});
+    return result.node;
   },
   insert: function(node) {
+    var nid = node.nid;
     Docs.insert({
-      _id: node.nid.toString(),
-      nid: node.nid,
-      title: node.title,
-      body: node.body
+      _id: nid,
+      node: node
     });
   },
   update: function(node) {
-    var id = node.nid.toString();
-    Docs.update(id, {
-      nid: node.nid,
-      title: node.title,
-      body: node.body
+    Docs.update(node.nid, {
+      node: node
     });
+  },
+  updateDrupal: function(url, node) {
+    this.unblock();
+    var result = Meteor.http.call("POST", url, node);
   }
 });
